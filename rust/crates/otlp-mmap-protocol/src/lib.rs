8e71855faa278e8c55c90d92c91b1464fcee18f3
Josh Suereth <Joshua.Suereth@gmail.com>
2026-02-15 12:21:57 -0500

    cargo fmt

--- rust/crates/otlp-mmap-protocol/src/lib.rs
+++ rust/crates/otlp-mmap-protocol/src/lib.rs
@@ -116,15 +116,7 @@ pub mod span_event {
         /// SpanKind is the type of span. Can be used to specify additional relationships between spans
         /// in addition to a parent/child relationship.
         #[derive(
-            Clone,
-            Copy,
-            Debug,
-            PartialEq,
-            Eq,
-            Hash,
-            PartialOrd,
-            Ord,
-            ::prost::Enumeration
+            Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration,
         )]
         #[repr(i32)]
         pub enum SpanKind {
@@ -329,17 +321,7 @@ pub struct Status {
 pub mod status {
     /// For the semantics of status codes see
     /// <https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/trace/api.md#set-status>
-    #[derive(
-        Clone,
-        Copy,
-        Debug,
-        PartialEq,
-        Eq,
-        Hash,
-        PartialOrd,
-        Ord,
-        ::prost::Enumeration
-    )]
+    #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
     #[repr(i32)]
     pub enum StatusCode {
         /// The default status.
@@ -873,4 +855,3 @@ impl SeverityNumber {
         }
     }
 }
-
